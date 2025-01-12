4af107d2eb4d690ddae4152243ebd682d94836ac
felixyman <68605654+felixyman@users.noreply.github.com>
2025-01-12 17:51:46 +0100

    Added two way binding (#1)
    * lagt till stöd för two-way-binding
    
    * fixat initproblem, rider/linuxstöd
    
    * feat: Remove setupChangeCallback
    
    * bump to dotnet 8
    
    * fix: Change typo
    
    ---------
    
    Co-authored-by: Felix Yman <Felix.Yman@capiostgoran.se>
    Co-authored-by: Andreas Sundström <andreas-sundstrom@hotmail.com>

--- SCE.Core/assets/index.ts
+++ SCE.Core/assets/index.ts
@@ -1,13 +1,25 @@
 import { EditorView, basicSetup } from './node_modules/codemirror/dist/index'
 import { EditorState } from './node_modules/@codemirror/state/dist/index'
 import { sql, SQLConfig, MSSQL } from './node_modules/@codemirror/lang-sql/dist/index'
-export { }
-type EditorsMap = {
-    [id: string]: EditorView
+
+type EditorObject = {
+    editor: EditorView,
+    dotnetReference: DotNetReference
 }
+
+type EditorsMap = {
+    [id: string]: EditorObject
+}
+
+// Add a type for our .NET reference
+type DotNetReference = {
+    invokeMethodAsync(methodName: string, ...args: any[]): Promise<void>;
+}
+
 enum Language {
     SQL
 }
+
 declare global {
     interface CodeEditor {
         CreateEditor: (parent: string, initialState: string, schema: any, theme: any) => void;
@@ -19,12 +31,16 @@ declare global {
     interface Window {
         CodeEditor: CodeEditor
     }
+
+    interface EditorView {
+        dotNetRef: string
+    }
 }
 
 window.CodeEditor = {
     Editors: {},
-    CreateEditor: (parent: string, textInput: string, schema?: any, language?: Language, theme?: any) => {
-        var extensions : Array<any> = [basicSetup];
+    CreateEditor: (parent: string, textInput: string, dotNetReference: DotNetReference, schema?: any, language?: Language, theme?: any) => {
+        var extensions: Array<any> = [basicSetup];
         if (language == Language.SQL) {
             var defaultConfig: SQLConfig = {
                 dialect: MSSQL,
@@ -32,39 +48,62 @@ window.CodeEditor = {
             };
             if (schema != null)
                 defaultConfig.schema = schema;
-
             extensions.push(sql(defaultConfig));
         }
         if (theme) {
             extensions.push(EditorView.theme(theme));
         }
+        
+        extensions.push(EditorView.updateListener.of((update) => {
+            if (update.docChanged) {
+                const editor = window.CodeEditor.Editors[parent]?.editor;
+                const dotNetRef = window.CodeEditor.Editors[parent]?.dotnetReference;
+                if (dotNetRef && editor) {
+                    dotNetRef.invokeMethodAsync('CodeChange', editor.state.doc.toString());
+                }
+            }
+        }));
+
         var initialState = EditorState.create({
             doc: textInput,
             extensions: extensions
         });
-
+        
         var editor = new EditorView({
             state: initialState,
             parent: document.getElementById(parent) as Element
-            
         });
         
-        //Add reference
         if (!window.CodeEditor.Editors)
             window.CodeEditor.Editors = {};
-        window.CodeEditor.Editors[parent] = editor as EditorView;
+
+
+        window.CodeEditor.Editors[parent] = {
+            editor: editor as EditorView,
+            dotnetReference: dotNetReference
+        } as EditorObject;
     },
+    
     GetStringValue: (parent: string) => {
-        var editor : EditorView | undefined = window.CodeEditor.Editors[parent];
+        var editor: EditorView | undefined = window.CodeEditor.Editors[parent]?.editor;
+
+        if (editor === undefined) {
+            throw new Error("No editor with id " + parent);
+        }
+
         return editor.state.doc.toString();
     },
+    
     SetStringValue: (parent: string, value: string) => {
-        var editor: EditorView | undefined = window.CodeEditor.Editors[parent];
-
-        editor.dispatch({ changes: { from: 0, to: editor.state.doc.length, insert: value } })
-        
+        var editor: EditorView | undefined = window.CodeEditor.Editors[parent]?.editor;
+        if (editor !== undefined) {
+            editor.dispatch({ changes: { from: 0, to: editor.state.doc.length, insert: value } })
+        }
     },
+    
     Dispose: (parent: string) => {
         delete window.CodeEditor.Editors[parent];
     }
 };
+
+export { }
